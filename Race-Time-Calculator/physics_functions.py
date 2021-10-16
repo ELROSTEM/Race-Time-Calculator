7267ad5a37f68e14b67df895df981f191cc0c3fe
Ryan Zhang <65325330+irrelevantRyan@users.noreply.github.com>
2021-10-15 23:15:10 -0400

    Structural Changes to the code base
    We are basing our code off the highschool ranker code. I got rid of the things we dont need and the things we can use. The physics function file will be used for the functions and the streamlit file will be used for the website that will be hosted on stream lit. I can work on streamlit while you (val) can work on the functons.

---
+++ Race-Time-Calculator/physics_functions.py
@@ -0,0 +1,38 @@
+import numpy
+
+###############################################################################
+# ^Python standard line length
+
+
+###############################################################################
+
+#Velocity
+
+def velocity_calc(time1, time2, displacement1, displacement2):
+    """Rename the functions if you want Val idk what to name these buy
+    they are just a template. I think this function would take in these 
+    parameters then we would jsut while loop it through the whole csv but let me
+    know how you think we should do it. None is just a stand in"""
+    return None
+
+def velocity_graph(velocity_data):
+    """We wound want a graph so I can display the things we calculated. Business0
+    people Like graphs and things that look complicated."""
+    return None
+
+
+
+###############################################################################
+
+#Acceleration
+
+def aceleration_calc():
+    """^ Thats not how you spell"""
+    return None
+
+def acceleration_graph():
+    """stuff"""
+    return None
+
+
+###############################################################################
