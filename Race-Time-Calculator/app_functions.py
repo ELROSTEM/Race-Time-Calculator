e9c1d28617fe0a4d81e95cb217a48bce66748c50
irrelevantRyan <65325330+irrelevantRyan@users.noreply.github.com>
2021-11-20 22:57:11 -0500

    Update

--- Race-Time-Calculator/app_functions.py
+++ Race-Time-Calculator/app_functions.py
@@ -63,6 +63,9 @@ def example_csv():
             ]
         }
     )
+
+    # example_csv = pd.read_csv('experimental_data.csv')
+
     example_csv = example_csv.to_csv().encode('utf-8')
 
     return example_csv
